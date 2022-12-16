71373a47df980ea0cc6124d664c6359f8edbcd09
Anji515 <anji.gd17@gmail.com>
2022-12-16 19:49:05 +0530

    fw21_0746_day-4

--- script/brands.js
+++ script/brands.js
@@ -1,5 +1,6 @@
-
-
+// import {nav} from './component/nav.js';
+// let navbar=document.getElementById('brandsNav');
+// navbar.innerHTML=nav();
 
 
 const alfabets= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','#']
@@ -23,8 +24,10 @@ for(let el of eachP){
 // MosueOver Events....
     el.addEventListener("mouseenter", () => { el.style.color = "rgb(75, 134, 243)"  });
     el.addEventListener("mouseenter", () => { el.style.fontSize = "16px"  });
+    el.addEventListener("mouseenter", () => { el.style.textDecoration = "underline"  });
     el.addEventListener('mouseleave', () => { el.style.color = "black" });
     el.addEventListener('mouseleave', () => { el.style.fontSize = "14px" });
+    el.addEventListener("mouseleave", () => { el.style.textDecoration = "none"  });
 
 // Click Event to newPage...    
         el.addEventListener('click',()=>{
