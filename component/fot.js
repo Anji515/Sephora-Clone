aedf9887c9c3126aafc72fc1c2d2898731ef1303
shaifali <shaifali02000@gmail.com>
2022-12-17 21:26:31 +0530

    done home page

---
+++ component/fot.js
@@ -0,0 +1,15 @@
+function fot(){
+    return `
+    <img src="./images/fot1.png" alt="">
+    <div>
+    <h4>Sign Up for SEPHORA Email</h4>
+    <input type="text" placeholder="Enter Your Email Address">
+    <button id="fotsignup">
+      <a href="signupLogin.html"> Sign Up</a>
+    </button>
+    </div>
+    <img src="./images/fot3.png" alt="">
+    `
+}
+
+export {fot}
